99c5b7b16ac7e02f11e3df470e42c39cb5b28756
AsmanAbdul <144958909+AsmanAbdul@users.noreply.github.com>
2025-04-19 21:28:49 +0300

    Update Pest.py

--- Pest.py
+++ Pest.py
@@ -5,3 +5,11 @@ class Pet:
         self.energy = 5   # Starting at midpoint
         self.happiness = 5  # Starting at midpoint
     
+def eat(self):
+        self.hunger = max(0, self.hunger - 3)
+        self.happiness = min(10, self.happiness + 1)
+        print(f"{self.name} has eaten.")
+
+    def sleep(self):
+        self.energy = min(10, self.energy + 5)
+        print(f"{self.name} had a good rest.")
