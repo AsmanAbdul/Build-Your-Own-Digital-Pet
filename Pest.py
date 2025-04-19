04152076a3ecc44de51063a1c8bd901312ffc540
Nompie Mthombeni Dube <64683159+Nompie1993@users.noreply.github.com>
2025-04-19 20:26:11 +0200

    Update Pet.py

---
+++ Pest.py
@@ -0,0 +1,7 @@
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 5  # Starting at midpoint
+        self.energy = 5   # Starting at midpoint
+        self.happiness = 5  # Starting at midpoint
+    
